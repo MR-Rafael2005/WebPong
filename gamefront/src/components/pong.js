bb1940c8fb0ba8fdc4e8d26eafa815fe55b05ebd
Mr. Rafael <ilderdamasceno@gmail.com>
2023-06-04 23:25:56 -0300

    Sistema de conectar e disconectar já prontos

--- gamefront/src/components/pong.js
+++ gamefront/src/components/pong.js
@@ -1,31 +1,29 @@
 import React from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import socketClient from "socket.io-client";
 
 const socket = socketClient("http://localhost:4000");
 
 const Pong = () => {
+    const [playersList, setPlyersList] = useState({});
 
     useEffect(() => {
         socket.on("connect", () => {
             console.log("Conectado");
         })
+    
+        socket.on("PlayerUpdate", (newPlayers) => {
+            setPlyersList(newPlayers);
+        })
     })
 
-    const players = {
-        player1: {
-            name: "PL1"
-        },
-        player2: {
-            name: "PL2"
-        }
-    }
+
 
     return(
         <div>
-            {Object.keys(players).map((key) => { 
+            {Object.keys(playersList).map((key) => { 
                 return(
-                    <div>{players[key].name}</div>
+                    <div>{playersList[key].name}</div>
                 )
             })}
         </div>
--- gameserver/server.js
+++ gameserver/server.js
@@ -2,9 +2,11 @@ import express from "express";
 import { Server, Socket } from "socket.io";
 import http from "http";
 
+//Http cria o server usando a função express, e o a variavel socket reconhece a criação de um server e usa o socket nesse server 
 const app = express();
 const server = http.createServer(app);
 const sockets = new Server(server, {
+    //Parte responsavel por permitir a execução em localhost
     cors: {
       origin: "*",
       methods: ["GET", "POST"],
@@ -13,11 +15,35 @@ const sockets = new Server(server, {
     }
 });
 
+const game = {
+  players: {
+
+  }
+}
+
+
+const updatePlayers = () => {
+  sockets.emit("PlayerUpdate", game.players);
+}
+
+//Função quando um player é conectado
 sockets.on("connection", (socket) => {
     console.log( socket.id + " foi conectado");
+
+    const name = "player_" + socket.id.substr(0,5);
+
+    game.players[socket.id] = { name };
+
+    //"Define um parametro" para quando o player(id) for desconctado
+    socket.on('disconnect', () => {
+      delete game.players[socket.id];
+      updatePlayers();
+    })
+
+    updatePlayers();
 })
 
-app.get("/", (req,res) => res.send("Oi mundo"));
+//app.get("/", (req,res) => res.send("Oi mundo"));
 
 const port = 4000;
 server.listen(port, () => console.log("Testando a porta " + port + " do server"));
